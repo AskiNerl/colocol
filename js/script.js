88f19e6e49ae64e649e3168d8b99a02e1c07fcce
Влад <vlados@example.com>
2025-12-20 23:49:57 +0200

    show

--- js/script.js
+++ js/script.js
@@ -38,3 +38,31 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     modal.classList.remove("show");
 });
+function createSnowflake() {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    
+
+    const size = Math.random() * 4 + 2 + 'px';
+    const left = Math.random() * 100 + 'vw';
+    const duration = Math.random() * 3 + 2 + 's';
+    const opacity = Math.random();
+
+    snowflake.style.width = size;
+    snowflake.style.height = size;
+    snowflake.style.left = left;
+    snowflake.style.opacity = opacity;
+    
+  
+    snowflake.style.animation = `fall ${duration} linear forwards`;
+    
+    document.body.appendChild(snowflake);
+
+
+    setTimeout(() => {
+        snowflake.remove();
+    }, parseFloat(duration) * 1000);
+}
+
+
+setInterval(createSnowflake, 100);
